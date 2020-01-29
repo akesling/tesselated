74395555485eb6a321d4328832fc58782699b7b5
Alex Kesling <alex@kesling.co>
2020-01-28 22:38:36 -0500

    Add some basic math operations for a simple Hex struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,81 @@
+
+
+pub struct Hex {
+    // Prevent independent construction so invariants are enforced.
+    _secret: (),
+    pub q: i32,
+    pub r: i32,
+    pub s: i32,
+}
+
+static HEX_DIRECTIONS: [Hex; 6] = [
+    Hex{_secret: (), r: 1, q: 0, s: -1},
+    Hex{_secret: (), r: 1, q: -1, s: 0},
+    Hex{_secret: (), r: 0, q: -1, s: 1},
+    Hex{_secret: (), r: -1, q: 0, s: 1},
+    Hex{_secret: (), r: -1, q: 1, s: 0},
+    Hex{_secret: (), r: 0, q: 1, s: -1},
+];
+
+pub enum HexDirections {
+    One = 0,
+    Two = 1,
+    Three = 2,
+    Four = 3,
+    Five = 4,
+    Six = 5,
+}
+
+impl Hex {
+    pub fn new(q: i32, r: i32, s: i32) -> Option<Hex> {
+        if q + r + s != 0 {
+            return None;
+        }
+
+        Some(Hex{_secret: (), q, r, s})
+    }
+
+    pub fn distance(&self, other: &Hex) -> i32 {
+        let hex_vec = self - other;
+
+        (hex_vec.q.abs() + hex_vec.r.abs() + hex_vec.s.abs()) / 2
+    }
+
+    pub fn neighbor(&self, dir: HexDirections) -> Hex {
+        self + &HEX_DIRECTIONS[dir as usize]
+    }
+}
+
+impl std::cmp::PartialEq for Hex {
+    fn eq(&self, other: &Hex) -> bool {
+        self.q == other.q && self.r == other.r && self.s == other.s
+    }
+}
+
+impl std::ops::Add<&Hex> for &Hex {
+    type Output = Hex;
+
+    fn add(self, other: &Hex) -> Hex {
+        Hex{_secret: (), q: self.q + other.q, r: self.r + other.r, s: self.s + other.s}
+    }
+}
+
+impl std::ops::Sub<&Hex> for &Hex {
+    type Output = Hex;
+
+    fn sub(self, other: &Hex) -> Hex {
+        Hex{_secret: (), q: self.q - other.q, r: self.r - other.r, s: self.s - other.s}
+    }
+}
+
+impl std::ops::Mul<&Hex> for &Hex {
+    type Output = Hex;
+
+    fn mul(self, other: &Hex) -> Hex {
+        Hex{_secret: (), q: self.q * other.q, r: self.r * other.r, s: self.s * other.s}
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
